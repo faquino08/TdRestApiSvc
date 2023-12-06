8799fe95bfe386403f8c269e4846ef4f24fcd5cb
faquino08 <faquino0896@gmail.com>
2023-12-05 20:01:41 -0500

    json.loads issue.

--- constants.py
+++ constants.py
@@ -7,5 +7,5 @@ POSTGRES_PORT = environ['POSTGRES_PORT']
 POSTGRES_DB = environ['POSTGRES_DB']
 POSTGRES_USER = environ['POSTGRES_USER']
 POSTGRES_PASSWORD = environ['POSTGRES_PASSWORD']
-DEBUG = json.loads(environ['DEBUG_BOOL'].lower())
+DEBUG = json.loads(environ['DEBUG_BOOL'].lower()) if len(environ['DEBUG_BOOL']) > 0 else False
 APP_NAME = environ.get('APP_NAME','TdQuote')
